3d5ea9cf691a9470cb97affc703b742b4dca23c3
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-08-03 00:54:27 +0200

    discordBot

---
+++ Practice/Vezba_14_DiscordBot/commands/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	Ping: require('./ping'),
+	ArgsInfo: require('./args-info'),
+	Help: require('./help'),
+	Commands: require('./commands'),
+	Addition: require('./addition'),
+	Math: require('./math'),
+};
