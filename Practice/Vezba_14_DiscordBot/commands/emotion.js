9211a8430bea4abf7ffc0d6697d657d046182e76
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-08-17 14:25:22 +0200

    discordbot emoticons

--- Practice/Vezba_14_DiscordBot/commands/emotion.js
+++ Practice/Vezba_14_DiscordBot/commands/emotion.js
@@ -9,7 +9,7 @@ module.exports = {
 	// eslint-disable-next-line no-unused-vars
 	execute(msg, args) {
        let firstArgument = args[0];
-       if(firstArgument == '/tableflip'){return msg.reply("╯°□°）╯︵ ┻━┻");}        
+       if(firstArgument == '/tableflip'){return msg.reply("(╯°□°）╯︵ ┻━┻");}        
        else if(firstArgument == '/unflip'){return msg.reply("┬─┬ ノ( ゜-゜ノ)");}
        else if(firstArgument == '/shrug'){return msg.reply("¯\\_(ツ)_/¯");}
        else{return msg.reply("Morate koristiti jednu od tri komande: /tableflip, /unflip ili /shrug. ");}
