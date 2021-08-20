1c18169e165210ab6e161a1bea10b5843c723f96
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-08-20 16:28:07 +0200

    List of books

--- Practice/React_in_progress/src/index.js
+++ Practice/React_in_progress/src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from 'react'
+import ReactDom from 'react-dom'
 
 // const Greeting = () => {
 //   return React.createElement('h1',{},'hello world');
 // }
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
 }
-ReactDom.render(<Greeting/>,document.getElementById('root'));
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/41yO95t41bL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+    alt=''
+  />
+)
+const Title = () => <h1>The Art of War</h1>
+const Author = () => <h4>Sun Tzu</h4>
+
+ReactDom.render(<BookList />, document.getElementById('root'))
