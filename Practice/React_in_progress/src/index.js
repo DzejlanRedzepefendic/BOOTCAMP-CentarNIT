a9765ac3afd683bc27b32e3141bc7fba89b1d03d
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-08-23 15:14:50 +0200

    props and props destructuring

--- Practice/React_in_progress/src/index.js
+++ Practice/React_in_progress/src/index.js
@@ -1,49 +1,48 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-// const Greeting = () => {
-//   return React.createElement('h1',{},'hello world');
-// }
+// css
 import './index.css'
+// var
+const books = [
+  {
+    id: 110,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41yO95t41bL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'The Art of War',
+    author: 'Sun Tzu',
+  },
+  {
+    id: 111,
+    img: 'https://www.delfi.rs/_img/artikli/2021/07/kovid_19_vv.jpg',
+    title: 'Covid 19+',
+    author: 'Marko Vidojković',
+  },
+  {
+    id: 112,
+    img: 'https://www.delfi.rs/_img/artikli/2019/08/the_little_prince_vv.jpg',
+    title: 'The Little Prince',
+    author: 'Antoine de Saint-Exupery',
+  },
+]
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} book={book}></Book>
+      })}
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props.book
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   )
 }
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/41yO95t41bL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
-    alt=''
-  />
-)
-const Title = () => <h1>The Art of War</h1>
-const Author = () => (
-  <h4 style={{ color: '#617d98', foontSize: '0.75rem', marginTop: '0.5rem' }}>
-    Sun Tzu
-  </h4>
-)
 
 ReactDom.render(<BookList />, document.getElementById('root'))
