d0528a4c48681eb8cae9fadfc2868b66d64fdd0c
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-08-24 08:32:06 +0200

    guessing number single/multi-player game

--- Projects/DiscordBot/commands/index.js
+++ Projects/DiscordBot/commands/index.js
@@ -1,11 +1,14 @@
 module.exports = {
-	Ping: require('./ping'),
-	ArgsInfo: require('./args-info'),
-	Help: require('./help'),
-	Commands: require('./commands'),
-	Addition: require('./addition'),
-	Math: require('./math'),
-	Guess: require('./guess'),
-	Emoticon: require('./emoticon'),
-	Calc: require('./calc')
-};
+  Ping: require('./ping'),
+  ArgsInfo: require('./args-info'),
+  Help: require('./help'),
+  Commands: require('./commands'),
+  Addition: require('./addition'),
+  Math: require('./math'),
+  Hangman: require('./calc'),
+  Guess: require('./guess'),
+  Emoji: require('./emoticon'),
+  Singlegame: require('./singlegame'),
+  Multigame: require('./multigame'),
+  Roll: require('./roll'),
+}
