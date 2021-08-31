9017b89580833248b8ddf890ae3fb53f02e6463f
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-08-31 17:06:56 +0200

    middleware

---
+++ Practice/node&express practice/02_express/final/08-middleware-basic.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+app.get('/', logger, (req, res) => {
+  res.send('Home')
+})
+app.get('/about', logger, (req, res) => {
+  res.send('About')
+})
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
