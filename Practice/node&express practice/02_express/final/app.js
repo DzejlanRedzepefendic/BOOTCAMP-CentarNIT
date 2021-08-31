e40436fdb687e86e0a313c28c6ff73bb1adeb448
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-08-31 20:52:22 +0200

    routes and controllers

---
+++ Practice/node&express practice/02_express/final/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const people = require('./routes/people')
+const auth = require('./routes/auth')
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
+
+app.use('/api/people', people)
+app.use('/login', auth)
+
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
+})
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
