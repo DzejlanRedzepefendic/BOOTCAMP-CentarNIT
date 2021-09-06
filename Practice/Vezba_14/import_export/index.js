567f325c13856a3507f7e76a20a0f939150f1b43
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-09-06 20:48:44 +0200

    fetch

--- Practice/Vezba_14/import_export/index.js
+++ Practice/Vezba_14/import_export/index.js
@@ -1,14 +1,20 @@
 import { nesto } from './test.js'
 
-let randomLetters = 'sfsfasfsafafadasdadasdads'
+// let randomLetters = 'sfsfasfsafafadasdadasdads'
 
-console.log(nesto(randomLetters))
+// console.log(nesto(randomLetters))
 
-try {
-  var data = await fetch('https://api.quotable.io/random')
-  const res = await data.json()
-  console.log(`${res.content} ~ ${res.author}`)
-} catch (error) {
-  console.log(error)
-}
+// try {
+//   var data = await fetch('https://api.quotable.io/random')
+//   const res = await data.json()
+//   console.log(`${res.content} ~ ${res.author}`)
+// } catch (error) {
+//   console.log(error)
+// }
 
+// get20Quotes().then(result => console.log(result)).catch((error)=>{console.log(error)})
+getRandomQuote('famous-quotes')
+  .then((result) => console.log(result))
+  .catch((error) => {
+    console.log(error)
+  })
--- Practice/Vezba_14/import_export/test.js
+++ Practice/Vezba_14/import_export/test.js
@@ -24,3 +24,34 @@ export const myPromise = new Promise((resolve, reject) => {
   }, 1000)
 })
 
+export async function getQoute() {
+  const result = await fetch('https://api.quotable.io/random')
+  const data = await result.json()
+  return data
+}
+
+export async function get20Quotes() {
+  try {
+    const result = await fetch('https://quotable.io/quotes?page=1')
+    const data = await result.json()
+
+    const formatedResult = data.results.map((element) => {
+      return { tags: element.tags, content: element.content }
+    })
+
+    return formatedResult
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getRandomQuote(tags) {
+  try {
+    const result = await fetch(`https://quotable.io/random?tags=${tags}`)
+    const data = await result.json()
+
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
